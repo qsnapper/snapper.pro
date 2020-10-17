9e318c5a60374b7e365133c0f9d5f2fe28301ca7
Quintus <qsnapper@gmail.com>
2020-10-17 22:57:51 +0200

    updated Woopra tracking code

--- assets/js/track.js
+++ assets/js/track.js
@@ -1,5 +1,6 @@
 if (document.location.hostname.search("snapper.pro") !== -1) {
-    // Start of Hotjar Code
+    
+  // Start of Hotjar Code
     (function (h, o, t, j, a, r) {
         h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
         h._hjSettings = { hjid: 1415912, hjsv: 6 };
@@ -9,14 +10,14 @@ if (document.location.hostname.search("snapper.pro") !== -1) {
         a.appendChild(r);
       })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
       // End of Hotjar Code
+
       // Start of Woopra Code
-      (function () {
-        var t, i, e, n = window, o = document, a = arguments, s = "script", r = ["config", "track", "identify", "visit", "push", "call", "trackForm", "trackClick"], c = function () { var t, i = this; for (i._e = [], t = 0; r.length > t; t++)(function (t) { i[t] = function () { return i._e.push([t].concat(Array.prototype.slice.call(arguments, 0))), i } })(r[t]) }; for (n._w = n._w || {}, t = 0; a.length > t; t++)n._w[a[t]] = n[a[t]] = n[a[t]] || new c; i = o.createElement(s), i.async = 1, i.src = "//static.woopra.com/js/w.js", e = o.getElementsByTagName(s)[0], e.parentNode.insertBefore(i, e)
+      (function(){
+        var t,i,e,n=window,o=document,a=arguments,s="script",r=["config","track","identify","visit","push","call","trackForm","trackClick"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src="//static.woopra.com/js/w.js",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)
       })("woopra");
-  
+    
       woopra.config({
-        domain: 'snapper.pro',
-        hide_campaign: true
+          domain: 'snapper.pro'
       });
       woopra.track();
       // End of Woopra Code
